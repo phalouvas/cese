95a4abb2c795d6d30b4db4ec262635e8d832904d
Panayiotis Halouvas <phalouvas@gmail.com>
2026-04-08 12:11:58 +0000

    feat: Add payment transaction ID handling in Registration, update status on payment authorization

--- cese/cese/doctype/registration/registration.py
+++ cese/cese/doctype/registration/registration.py
@@ -22,3 +22,29 @@ class Registration(Document):
 
 		self.grand_total = ticket_data.grand_total
 		self.currency = ticket_data.currency
+
+	def on_payment_authorized(self, status=None):
+		if status not in ("Authorized", "Completed"):
+			return
+
+		transaction_id = frappe.db.get_value(
+			"Integration Request",
+			{
+				"reference_doctype": self.doctype,
+				"reference_docname": self.name,
+				"integration_request_service": "PayPal Checkout",
+				"status": "Completed",
+			},
+			"request_id",
+			order_by="modified desc",
+		)
+
+		self.db_set(
+			{
+				"status": "Paid",
+				"payment_transaction_id": transaction_id,
+			},
+			update_modified=False,
+		)
+
+		return frappe.utils.get_url(f"/registration?name={self.name}")
