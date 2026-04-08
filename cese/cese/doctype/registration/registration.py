d6b6616e05b8451806d36d130a86b19dbd6e6150
Panayiotis Halouvas <phalouvas@gmail.com>
2026-04-08 06:58:51 +0000

    feat: Enhance Registration validation and update web form configuration

--- cese/cese/doctype/registration/registration.py
+++ cese/cese/doctype/registration/registration.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2026, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class Registration(Document):
-	pass
+	def validate(self):
+		if not self.ticket:
+			self.amount = None
+			return
+
+		ticket_amount = frappe.db.get_value("Tickets", self.ticket, "amount")
+
+		if ticket_amount is None:
+			frappe.throw(_("Ticket {0} does not exist.").format(frappe.bold(self.ticket)))
+
+		self.amount = ticket_amount
--- cese/cese/web_form/registration/registration.js
+++ cese/cese/web_form/registration/registration.js
@@ -1,3 +1,25 @@
 frappe.ready(function() {
-	// bind events here
+	const setTicketAmount = () => {
+		const ticket = frappe.web_form.get_value("ticket")
+
+		if (!ticket) {
+			frappe.web_form.set_value("amount", null)
+			return
+		}
+
+		frappe.call({
+			method: "frappe.client.get_value",
+			args: {
+				doctype: "Tickets",
+				filters: { name: ticket },
+				fieldname: "amount"
+			},
+			callback: (r) => {
+				frappe.web_form.set_value("amount", r.message ? r.message.amount : null)
+			}
+		})
+	}
+
+	frappe.web_form.on("ticket", setTicketAmount)
+	setTicketAmount()
 })
